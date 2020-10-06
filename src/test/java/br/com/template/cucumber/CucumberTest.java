81fcb3c6138482be700e414baddc8c879004f679
eosilva <efraim.silvacastgroup.com.br>
2020-10-06 14:32:19 -0300

    First Commit of Project

---
+++ src/test/java/br/com/template/cucumber/CucumberTest.java
@@ -0,0 +1,12 @@
+package br.com.template.cucumber;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "pretty", features = "src/test/features")
+public class CucumberTest {
+
+}
---
+++ src/test/java/br/com/template/model/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.template.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
